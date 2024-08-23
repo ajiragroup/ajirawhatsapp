e44181a41b2bf9f3ab0ea2a7b7f58c61938efad5
ajiragroup <108009061+ajiragroup@users.noreply.github.com>
2024-08-23 16:27:41 +0545

    Update whatsapp_message.py

--- ultramsg_4_erpnext/ultramsg_4_erpnext/doctype/whatsapp_message/whatsapp_message.py
+++ ultramsg_4_erpnext/ultramsg_4_erpnext/doctype/whatsapp_message/whatsapp_message.py
@@ -2,36 +2,40 @@
 # For license information, please see license.txt
 
 import frappe
+
 # from frappe.model.document import Document
 import requests
 import frappe
 from frappe.model.document import Document
-class whatsappmessage(Document):  
- @frappe.whitelist()
- def msg(self,token, recipient,message_url,):
-#    message_url=message_url
-    messagetosend = {
-     "type": "text",
-     "text": messagebody
-     }
-   payload = {
-        'message': messagetosend,
-        'src.name': srcname,
-        'channel': "whatsapp",
-        'source': source,
-        'destination': recipient,
-        'disablePreview' : 'False"
-       }
-    
-   headers = {
-    'content-type': 'application/x-www-form-urlencoded',
-    'accept': 'application/json',
-    'apikey':apikey
-   }
 
-   try:
-           response = requests.post(message_url, data=payload, headers=headers)
-           return response.text
-   except Exception as e:
-          return e
-   
+
+class whatsappmessage(Document):
+    @frappe.whitelist()
+    def msg(
+        self,
+        token,
+        recipient,
+        message_url,
+    ):
+        #    message_url=message_url
+        messagetosend = {"type": "text", "text": messagebody}
+        payload = {
+            "message": messagetosend,
+            "src.name": srcname,
+            "channel": "whatsapp",
+            "source": source,
+            "destination": recipient,
+            "disablePreview": "False",
+        }
+
+        headers = {
+            "content-type": "application/x-www-form-urlencoded",
+            "accept": "application/json",
+            "apikey": apikey,
+        }
+
+        try:
+            response = requests.post(message_url, data=payload, headers=headers)
+            return response.text
+        except Exception as e:
+            return e
